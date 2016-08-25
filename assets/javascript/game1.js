4c4500f4af69e2ecb3934eedecb51be1db9bfab1
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2016-08-24 20:56:04 -0500

    attempting to rebuild

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,31 +1,50 @@
-var correctLetter = []
 
-var wordList = 'duncan';
-console.log(correctLetter.length, wordList.length)
+var wordList = ['duncan', 'parker','ginobili','popovich','leonard'];
+var currentWord;
+var wordArray = []; 
+var lettersGuessed ;
+var remainingGuesses = 25;
 
-document.onkeyup = function(event){
-	userGuess= String.fromCharCode(event.keyCode).toLowerCase();
-		// console.log('working');
+var x = Math.floor(Math.random() * (5 - 0)) ;
 
+document.onkeyup = function(event){	
 
-if ( correctLetter.length === wordList.length ){
-	console.log("functionstoping")
-	alert("you win")
+var userGuess= String.fromCharCode(event.keyCode).toUpperCase();
+
+	$('#guessed-letters').append(' ' + userGuess + " - ");
+	$('#guesses-remaining').empty();
+	$('#guesses-remaining').html("Guesses Remaining: " + remainingGuesses);
+	remainingGuesses--;	
+
+	if(remainingGuesses <= 0){
+		$('#guesses-remaining').html("Guesses Remaining: " + "Game Over");
+		$('#guessed-letters').html("Letters Guessed: " + 'You Lose');
+	}
+		
+
+	for (var i = 0; i < wordList.length; i++) {
+	
+		currentWord.push(wordList[x]);	
+	
+		console.log('currentWord'+ ' ' + currentWord);
+	}
+	
+	console.log(currentWord);
 }
-// 'parker','ginobili','popovich','leonard';
 
-	for (var i = 0; i < 5; i++) {
-		console.log(userGuess + '===' + wordList[i]);
-		if (userGuess === wordList[i].toLowerCase()){
-			// console.log('Letter Appears');
-			$("#words"). append("<div>"+ userGuess + "</div>")
 
-			correctLetter.push(wordList[i])
-		 }
+	// for (var i = 0; i < 5; i++) {
+	// 	// console.log(userGuess + '===' + wordList[i]);
+	// 	if (userGuess === wordList[i].toLowerCase()){
+	// 		// console.log('Letter Appears');
+	// 		$("#words"). append("<div>"+ userGuess + "</div>")
+
+	// 		correctLetter.push(wordList[i])
+	// 	 }
 		 
 
 
-		 }
+	// 	 }
 
  
 
@@ -42,4 +61,4 @@ if ( correctLetter.length === wordList.length ){
 
 
 
-}
+// }
