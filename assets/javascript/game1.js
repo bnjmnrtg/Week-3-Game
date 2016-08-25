38c5c183c0e4427992af6011e7f750f80b2cadcd
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2016-08-24 23:48:25 -0500

    fixing Game

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,51 +1,117 @@
+var wordList = ['DUNCAN', 'PARKER', 'GINOBILI', 'POPOVICH', 'LEONARD'];
+var currentWord = [];
+var split = [];
+var correctLetter = [];
+var lettersGuessed = [];
+var remainingGuesses = 10;
+var wins = 0;
+var loses = 0;
+var dashes = [];
+var updatedDisplay;
 
+document.onkeyup = function(event) {
+    playGame();
 
-var wordList = ['duncan', 'parker','ginobili','popovich','leonard'];
-var currentWord;
-var wordArray = []; 
-var lettersGuessed ;
-var remainingGuesses = 25;
+};
+// ======================================================================================================
+function playGame() {
+    userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+    lettersGuessed.push(userGuess);
+    remainingGuesses--;
 
-var x = Math.floor(Math.random() * (5 - 0)) ;
+    // This Will Display Letters and # of Guesses
+    $('#guessed-letters').append(' ' + userGuess + " - ");
+    $('#guesses-remaining').empty();
+    $('#guesses-remaining').html("Guesses Remaining: " + remainingGuesses);
 
-document.onkeyup = function(event){	
+    if (remainingGuesses === 0) {
+        loses++;
+        $('#loss').empty();
+        $('#loss').html("Loses: " + loses);
+    }
 
-var userGuess= String.fromCharCode(event.keyCode).toUpperCase();
+    if (remainingGuesses <= 0) {
+        $('#guesses-remaining').html("Guesses Remaining: " + "Game Over");
+        $('#guessed-letters').html("Letters Guessed: " + 'You Lose');
+        $('#loss').empty();
+        $('#loss').html("Loses: " + loses);
+        $('#alerts').html("Instruction: Press Any Key To Continue To Play");
 
-	$('#guessed-letters').append(' ' + userGuess + " - ");
-	$('#guesses-remaining').empty();
-	$('#guesses-remaining').html("Guesses Remaining: " + remainingGuesses);
-	remainingGuesses--;	
+    }
 
-	if(remainingGuesses <= 0){
-		$('#guesses-remaining').html("Guesses Remaining: " + "Game Over");
-		$('#guessed-letters').html("Letters Guessed: " + 'You Lose');
-	}
-		
+    if (remainingGuesses < 0) {
+        reset();
+    }
+    // ================  Selects Random Word ============================= 
 
-	for (var i = 0; i < wordList.length; i++) {
-	
-		currentWord.push(wordList[x]);	
-	
-		console.log('currentWord'+ ' ' + currentWord);
-	}
-	
-	console.log(currentWord);
+    for (var i = 0; i < 1; i++) {
+        var x = Math.floor(Math.random() * (5 - 0));
+        // console.log("Random wordList" + " " + wordList[x]);
+        currentWord.push(wordList[x]);
+    };
+
+    for (var i = 0; i < currentWord.length; i++) {
+        split = currentWord[0].split('');
+    };
+
+    console.log('currentWord' + ' ' + split);
+    // ===================  Checks and Pushes Correct Letters ====================================================
+    for (var i = 0; i < split.length; i++) {
+        console.log(userGuess === split[i]);
+        if (userGuess === split[i]) {
+            correctLetter.push(userGuess);
+            console.log("Correct Letters" + " " + correctLetter)
+            $('#current-word').append(userGuess);
+        }
+        // ================== Verifys If That You won the Game ==============================
+        if (split.length === correctLetter.length) {
+            wins++;
+            $('#win').html("Wins: " + wins);
+            reset();
+        }
+
+    };
+
 }
 
+function updateDisplay(letter) {
+    for (i = 0; i < split.length; i++) {
+        if (split[i] === letter) {
+            split.splice(i, 1, letter);
+        }
+    }
+    updatedDisplay = dashes.join('');
+    $('#current-word').html(updatedDisplay);
+};
 
-	// for (var i = 0; i < 5; i++) {
-	// 	// console.log(userGuess + '===' + wordList[i]);
-	// 	if (userGuess === wordList[i].toLowerCase()){
-	// 		// console.log('Letter Appears');
-	// 		$("#words"). append("<div>"+ userGuess + "</div>")
+// =========== This Is The Reset Function That Automatically Trigers When You LOSE
+function reset() {
+    currentWord = [];
+    split = [];
+    correctLetter = [];
+    lettersGuessed = [];
+    currentWord ;
+    remainingGuesses = 10;
+    $('#current-word').html("Word: " + correctLetter);
+    $('#guesses-remaining').html("Guesses Remaining: ");
+    $('#guessed-letters').html("Letters Guessed: ");
+    
 
-	// 		correctLetter.push(wordList[i])
-	// 	 }
-		 
+    playGame();
+};
+
+// for (var i = 0; i < 5; i++) {
+// 	// console.log(userGuess + '===' + wordList[i]);
+// 	if (userGuess === wordList[i].toLowerCase()){
+// 		// console.log('Letter Appears');
+// 		$("#words"). append("<div>"+ userGuess + "</div>")
+
+// 		correctLetter.push(wordList[i])
+// 	 }
 
 
-	// 	 }
+
+// 	 }
 
 // var correctLetter = []
 
@@ -71,27 +137,26 @@ var userGuess= String.fromCharCode(event.keyCode).toUpperCase();
 
 // 			correctLetter.push(wordList[i])
 // 		 }
-		 
+
 
 
 // 		 }
 
 
- 
 
 
-	// var letterIndex= word1.indexOf(userGuess); 
-	// 	console.log(''+letterIndex);
+
+// var letterIndex= word1.indexOf(userGuess); 
+// 	console.log(''+letterIndex);
 
 
-	// 		var spanToFill = document.getElementById(''+letterIndex);
-	// 	console.log(spanToFill);
+// 		var spanToFill = document.getElementById(''+letterIndex);
+// 	console.log(spanToFill);
 
 
-	
+
 
 
 
 
 // }
-
